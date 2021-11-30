4729f9ebc345a6f3846ca5b0f0451df7ab2b6fb9
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2021-11-30 22:59:04 +0900

    保存３回目

--- html_kitaoka_08/apps/js/app2.js
+++ html_kitaoka_08/apps/js/app2.js
@@ -1,32 +1,44 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
-// // const todos = JSON.parse(localStorage.getItem("todos"));
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-// if(todos) {
-//     todos.forEach(todo => {
-//         add(todo);
-//     });
-// };
-
-form.addEventListener("submit", (event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     add();
 });
 
-const add = () => {
+const add = (todo) => {
     let todoText = input.value;
 
-    // if(todo){
-    //     todoText = todo;
-    // }
+    if(todo){
+        todoText = todo.text;
+    }
 
     if(todoText.length > 0) {
         //liタグを作成し、フォームに入力した値を取得する
         const li = document.createElement("li");
         li.innerText = todoText;
-        //liタグにデザインを当てるためにクラスを指定する
+        //liタグにbootstrapでデザインを当てるためにクラスを指定する
         li.classList.add("list-group-item");
+
+        if(todo && todo.completed) {
+            li.classList.add("text-decoration-line-through");
+        }
+
+        li.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
+
+        li.addEventListener("click", () => {
+            //bootstrapで打消し線をつけるクラスを指定する
+            //指定のクラスがあれば削除、なければ追加で打消し線を入れたり消したりする
+            li.classList.toggle("text-decoration-line-through");
+            saveData();
+        });
+
         ul.appendChild(li);
         input.value = "";
         saveData();
@@ -36,8 +48,20 @@ const add = () => {
 const saveData = () => {
     const lists = document.querySelectorAll("li");
     let todos = [];
-    lists.forEach(list => {
-        todos.push(list.innerText);
+    //forEachのラベルにlistを指定（変数）
+    lists.forEach((list) => { 
+        let todo = {
+            text: list.innerText,
+            completed: list.classList.contains("text-decoration-line-through")
+        };
+        //配列にまとめ直してローカルストレージに保存する
+        todos.push(todo);
     });
     localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+if(todos.length>0) {
+    todos.forEach((todo) => {
+        add(todo);
+    });
 };
