6d2fb3ddb77b06564b48e5be66a84a956f88f922
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-01-08 03:03:20 +0900

    JS課題4

--- js④_kitaoka_08/weather.js
+++ js④_kitaoka_08/weather.js
@@ -1,6 +1,23 @@
 let YOUR_ACCESS_KEY = "iWsuU79G1a04z9F0EYxuaGDUENuveGcg3nIXcAZY_jU";
 let endpoint = `https://api.unsplash.com/photos/random/?client_id=${YOUR_ACCESS_KEY}`;
 
+let imageElement = document.querySelector("#unsplashImage");
+let imageLink = document.querySelector("#imageLink");
+
+fetch(endpoint)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+        // console.log(jsonData);
+        imageElement.src = jsonData.urls.regular;
+        // console.log(jsonData.urls.full);
+        imageLink.setAttribute("href", jsonData.links.html);
+    })
+    .catch(function(error){
+        console.log("Error:" + error);
+    });
+
 let weather = {
     "API_KEY": "7fdddc19aef6824dd784a69c3e714777",
     fetchweather: function(city){
@@ -22,28 +39,16 @@ let weather = {
                 document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
                 document.querySelector(".description").innerText = description;
                 document.querySelector(".temp").innerText = temp +"°C";
-                document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+                document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
                 document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         })
-    }, 
-}
+    },
+    search: function(){
+        this.fetchweather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+})
 
-
-
-let imageElement = document.querySelector("#unsplashImage");
-let imageLink = document.querySelector("#imageLink");
-
-fetch(endpoint)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(jsonData){
-        // console.log(jsonData);
-        imageElement.src = jsonData.urls.regular;
-        // console.log(jsonData.urls.full);
-        imageLink.setAttribute("href", jsonData.links.html);
-    })
-    .catch(function(error){
-        console.log("Error:" + error);
-    });
 
