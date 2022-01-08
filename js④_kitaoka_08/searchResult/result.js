4e89f001dcf084477bf9022d71921700541a0055
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-01-08 09:52:37 +0900

    JS課題4

--- js④_kitaoka_08/searchResult/result.js
+++ js④_kitaoka_08/searchResult/result.js
@@ -19,25 +19,8 @@ fetch(endpoint)
         console.log("Error:" + error);
     });
 
-//検索ボタンをクリックすると、検索バーに入力した都市の天気が表示される処理
-document.querySelector(".search button").addEventListener("click", function(){
-    weather.search();
-    aftersearch();
-    
-})
-//検索バーに入力後Enterキーを押すと入力した都市の天気が表示される処理
-document.querySelector(".search-bar").addEventListener("keyup", function(event){
-    if(event.key == "Enter"){
-        weather.search();
-        aftersearch();
-    }
-})
-
-function aftersearch(){
     const input = document.querySelector(".search-bar");
     const li = document.createElement("li");
     li.innerText = input.value;
     li.classList.add("li");
     ul.appendChild(li);
-    input.value = "";
-}
