78a08aae2dace10edf8367546666ded9a97f345f
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-01-08 09:36:21 +0900

    JS課題4

--- js④_kitaoka_08/searchResult/result.js
+++ js④_kitaoka_08/searchResult/result.js
@@ -1,3 +1,24 @@
+const YOUR_ACCESS_KEY = "iWsuU79G1a04z9F0EYxuaGDUENuveGcg3nIXcAZY_jU";
+const endpoint = `https://api.unsplash.com/photos/random?client_id=${YOUR_ACCESS_KEY}`;
+
+const imageElement = document.querySelector("#unsplashImage");
+const imageLink = document.querySelector("#imageLink");
+
+//unsplashAPIのjsonデータを取得・表示する処理
+fetch(endpoint)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+        // console.log(jsonData);
+        imageElement.src = jsonData.urls.regular;
+        // console.log(jsonData.urls.full);
+        imageLink.setAttribute("href", jsonData.links.html);
+    })
+    .catch(function(error){
+        console.log("Error:" + error);
+    });
+
 //検索ボタンをクリックすると、検索バーに入力した都市の天気が表示される処理
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
